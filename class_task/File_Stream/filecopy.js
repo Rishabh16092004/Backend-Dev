73c7467a959cc09104b567dea6fbfc5a38c912ac
Rishabh16092004 <rishabhjaiswal16092004@gmail.com>
2026-01-27 11:34:04 +0530

    commit changes

--- class_task/File_Stream/filecopy.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs")
-
-const {Transform} = require("stream")
-
-fs.createReadStream("./input.txt")                                                                                                                 
-.pipe(fs.createWriteStream("./output.txt"));
--- class_task/File_Stream/writefile.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs")
-
-const writeStream = fs.createWriteStream("./sample2.txt")
-
-writeStream.write("Hello GLA\nWelcome to Stream\n");
-// writeStream.write("Welcome to Stream\n");
-
-writeStream.end();
